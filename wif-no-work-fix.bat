3ef088ce1698e06e894cc534526a653ebd97391f
Brandon <74426342+Brandonbr1@users.noreply.github.com>
2022-01-03 07:14:58 -0800

    Add files via upload

---
+++ wif-no-work-fix.bat
@@ -0,0 +1,19 @@
+ipconfig /release
+
+ipconfig /renew
+
+netsh winsock reset
+
+netsh int ip reset
+
+ipconfig /flushdns
+
+ipconfig /registerdns
+
+netsh int tcp set heuristics disabled
+
+netsh int tcp set global autotuninglevel=disabled
+
+netsh int tcp set global rss=enabled
+
+netsh int tcp show global
